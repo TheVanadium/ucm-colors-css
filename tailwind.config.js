5a8e7153b28db912a45d3af2bb414645148e8049
Garret Castro <53013571+TheVanadium@users.noreply.github.com>
2025-04-29 14:27:02 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Primary
+        'bobcat-blue': '#0F2D52',
+        'bobcat-gold': '#DAA900',
+
+        // Secondary
+        'sierra-sky-blue': '#0091B3',
+        'golden-wheat': '#FFBF3C',
+        'deep-sky-blue': '#005487',
+        'half-dome-slate': '#5B5B5B',
+        'beginnings-medium-grey': '#E5E5E5',
+        'beginnings-light-grey': '#EFEFEF',
+
+        // Accent
+        'accent-green': '#6BA43A',
+        'accent-orange': '#F18A00',
+        'accent-blue': '#99D9D9',
+      },
+    },
+  },
+  plugins: [],
+}
